1ad361713a4659da51b682c9acd22322a49a5d7f
”Kyrylo <kirillartemov1993@gmail.com>
2023-03-27 12:36:41 +0200

    created main class

---
+++ src/main/java/ua/foxminded/javaspring/ThePlacesOfRacers.java
@@ -0,0 +1,11 @@
+package ua.foxminded.javaspring;
+
+import java.io.FileNotFoundException;
+
+public class ThePlacesOfRacers {
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+	}
+
+}

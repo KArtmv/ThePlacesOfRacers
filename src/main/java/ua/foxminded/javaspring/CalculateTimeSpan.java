483a0d6486b37d3d43c32272e713e3c547aeca5e
”Kyrylo <kirillartemov1993@gmail.com>
2023-03-27 16:47:01 +0200

    moved timeOfLap into the method
    recorded formattedDuration instead concatenate strings String.format

--- src/main/java/ua/foxminded/javaspring/CalculateTimeSpan.java
+++ src/main/java/ua/foxminded/javaspring/CalculateTimeSpan.java
@@ -4,16 +4,16 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 public class CalculateTimeSpan {
 
-	Map<String, ArrayList<String>> timeOfLap = new HashMap<>();
-
-	public Map<String, ArrayList<String>> calculateTimeSpan(Map<String, ArrayList<LocalDateTime>> inputTimeOfLap) {
-		for (Entry<String, ArrayList<LocalDateTime>> entry : inputTimeOfLap.entrySet()) {
-			ArrayList<LocalDateTime> val = entry.getValue();
+	public Map<String, List<String>> calculateTimeSpan(Map<String, List<LocalDateTime>> timeOfRacers) {
+		Map<String, List<String>> timeOfLap = new HashMap<>();
+		for (Entry<String, List<LocalDateTime>> entry : timeOfRacers.entrySet()) {
+			List<LocalDateTime> val = entry.getValue();
 			LocalDateTime timeOfStarted = val.get(0);
 			LocalDateTime timeOfEnded = val.get(1);
 
@@ -27,17 +27,11 @@ public class CalculateTimeSpan {
 
 			long diffInMilli = fromTemp.until(timeOfEnded, ChronoUnit.MILLIS);
 
-			String formattedDuration = diffInMinutes + ":" + String.format("%02d", diffInSeconds) + "."
-					+ String.format("%03d", diffInMilli);
+			String formattedDuration = String.format("%s:%02d.%03d", diffInMinutes, diffInSeconds, diffInMilli);
 
-			ArrayList<String> time = new ArrayList<>();
-
-			time.add(formattedDuration);
-			timeOfLap.put(entry.getKey(), time);
+			timeOfLap.computeIfAbsent(entry.getKey(), k -> new ArrayList<>()).add(formattedDuration);
 
 		}
-
 		return timeOfLap;
 	}
-
 }

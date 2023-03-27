479c9aec6ce93745badc6db5424cfa931a38125c
”Kyrylo <kirillartemov1993@gmail.com>
2023-03-27 16:33:40 +0200

    recoded
    method not take parametric
    changed try to try-with-resources
    added logging error
    added call method for calculate period between two times

--- src/main/java/ua/foxminded/javaspring/TakeTimeOfRacers.java
+++ src/main/java/ua/foxminded/javaspring/TakeTimeOfRacers.java
@@ -6,44 +6,40 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TakeTimeOfRacers {
 
-	Map<String, ArrayList<LocalDateTime>> timeOfRacers = new HashMap<>();
+	public Map<String, List<String>> takeTimeOfRacers() {
+		Map<String, List<LocalDateTime>> timeOfRacers = new HashMap<>();
 
-	public Map<String, ArrayList<LocalDateTime>> takeTimeOfRacers(String fileWithTime) throws FileNotFoundException {
-		Scanner scTimeOfStart = null;
-		try {
-			scTimeOfStart = new Scanner(new File(fileWithTime));
-			while (scTimeOfStart.hasNextLine()) {
-				String input = scTimeOfStart.nextLine();
-				Pattern pattern = Pattern.compile("(\\w+)(\\d{4}-\\d{2}-\\d{2}_\\d{2}:\\d{2}:\\d{2}\\.\\d{3})");
-				Matcher matcher = pattern.matcher(input);
-				if (matcher.matches()) {
-					String racer = matcher.group(1);
-					String time = matcher.group(2).replaceAll("_", " ");
+		String[] fileNames = { "src/main/resources/start.log", "src/main/resources/end.log" };
+		for (String fileName : fileNames) {
+			try (Scanner scanner = new Scanner(new File(fileName))) {
+				while (scanner.hasNextLine()) {
+					String input = scanner.nextLine();
+					Pattern pattern = Pattern.compile("(\\w+)(\\d{4}-\\d{2}-\\d{2}_\\d{2}:\\d{2}:\\d{2}\\.\\d{3})");
+					Matcher matcher = pattern.matcher(input);
+					if (matcher.matches()) {
+						String racer = matcher.group(1);
+						String time = matcher.group(2).replace("_", " ");
+						LocalDateTime dateTime = LocalDateTime.parse(time,
+								DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS"));
 
-					DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-					LocalDateTime dateTime = LocalDateTime.parse(time, formatter);
-					ArrayList<LocalDateTime> timeOfRacerArray = new ArrayList<>();
-					timeOfRacerArray.add(dateTime);
-					timeOfRacers.put(racer, timeOfRacerArray);
+						timeOfRacers.computeIfAbsent(racer, k -> new ArrayList<>()).add(dateTime);
+					}
 				}
-
-			}
-		} catch (FileNotFoundException e) {
-		} catch (NoSuchElementException e) {
-		} finally {
-			if (scTimeOfStart != null) {
-				scTimeOfStart.close();
+			} catch (FileNotFoundException e) {
+				System.err.println("File not found: " + fileName);
 			}
 		}
 
-		return timeOfRacers;
+		CalculateTimeSpan calculateTimeSpan = new CalculateTimeSpan();
+		
+		return calculateTimeSpan.calculateTimeSpan(timeOfRacers);
 	}
 }

3cbdfe03e9dd4455b352e2e44d1ee21988bf2c6a
”Kyrylo <kirillartemov1993@gmail.com>
2023-03-27 16:12:07 +0200

    added to call PrintRaport

--- src/main/java/ua/foxminded/javaspring/ThePlacesOfRacers.java
+++ src/main/java/ua/foxminded/javaspring/ThePlacesOfRacers.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 public class ThePlacesOfRacers {
 
 	public static void main(String[] args) throws FileNotFoundException {
-
+		PrintRaport printRaport = new PrintRaport();
+		printRaport.printRaport();
 	}
-
 }

6fdafcabd31b805ec704afb3cb4c3afd0eb31ffd
”Kyrylo <kirillartemov1993@gmail.com>
2023-03-27 16:10:44 +0200

    renamed

---
+++ src/main/java/ua/foxminded/javaspring/RaceReportModifier.java
@@ -0,0 +1,40 @@
+package ua.foxminded.javaspring;
+
+import java.io.FileNotFoundException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class RaceReportModifier {
+
+	public Map<Integer, String> raceReportModifier() throws FileNotFoundException {
+		RaceResultsSorter raceResultsSorter = new RaceResultsSorter();
+		Map<String, List<String>> timeOfLap = raceResultsSorter.raceResultsSorter();
+
+		CalculateMaxLengthNameAndTeam lengthOfString = new CalculateMaxLengthNameAndTeam();
+		List<Integer> integers = lengthOfString.calculateMaxLengthNameAndTeam(timeOfLap);
+		int lengthOfName = integers.get(0);
+		int lengthOfTeam = integers.get(1);
+
+		Map<Integer, String> timesOfLap = new LinkedHashMap<>();
+		int i = 1;
+
+		for (Entry<String, List<String>> entry : timeOfLap.entrySet()) {
+			String time = entry.getKey();
+			List<String> val = entry.getValue();
+			String name = val.get(0);
+			String team = val.get(1);
+			Repeat repeat = new Repeat();
+			StringBuilder view = new StringBuilder();
+			view.append(name).append(repeat.repeat(" ", lengthOfName - name.length())).append("|").append(team)
+					.append(repeat.repeat(" ", lengthOfTeam - team.length())).append("|").append(time);
+
+			timesOfLap.put(i, view.toString());
+
+			i++;
+		}
+		return timesOfLap;
+
+	}
+}

8641e12b9f3db6e04a97eb90a627de28bd8abbd0
”Kyrylo <kirillartemov1993@gmail.com>
2023-03-27 12:04:20 +0200

    renamed

--- src/main/java/ua/foxminded/javaspring/ModifyBeforePrintReport.java
+++ src/main/java/ua/foxminded/javaspring/ModifyBeforePrintReport.java
@@ -1,19 +1,24 @@
 package ua.foxminded.javaspring;
 
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 
-public class PrintReport {
+public class ModifyBeforePrintReport {
 
-	Map<String, ArrayList<String>> timesOfLap = new LinkedHashMap<>();
+	public void modifyBeforePrintReport() throws FileNotFoundException {
+		SotrByTime sotrByTime = new SotrByTime();
+		Map<String, List<String>> timeOfLap = sotrByTime.sotrByTime();
 
-	public void printReport(Map<String, ArrayList<String>> input) {
+		
+		Map<String, List<String>> timesOfLap = new LinkedHashMap<>();
 		int i = 1;
 		int lengthOfNameMAX = 0;
 		int lengthOfTeamMAX = 0;
-		for (Map.Entry<String, ArrayList<String>> entry : input.entrySet()) {
-			ArrayList<String> val = entry.getValue();
+		for (Entry<String, List<String>> entry : timeOfLap.entrySet()) {
+			List<String> val = entry.getValue();
 			String time = entry.getKey();
 			String name = val.get(0);
 			String team = val.get(1);
@@ -36,21 +41,22 @@ public class PrintReport {
 			i++;
 		}
 
-		for (Map.Entry<String, ArrayList<String>> entry : timesOfLap.entrySet()) {
+		for (Map.Entry<String, List<String>> entry : timesOfLap.entrySet()) {
 			String key = entry.getKey();
-			ArrayList<String> val = entry.getValue();
+			List<String> val = entry.getValue();
 			String name = val.get(0);
 			String team = val.get(1);
 			String time = val.get(2);
 
 			Repeat repeat = new Repeat();
-			String view = key + name + repeat.repeat(" ", lengthOfNameMAX - name.toCharArray().length) + team
-					+ repeat.repeat(" ", lengthOfTeamMAX - team.toCharArray().length) + time;
 
+			StringBuilder view = new StringBuilder();
+			view.append(key).append(name).append(repeat.repeat(" ", lengthOfNameMAX - name.length())).append(team)
+					.append(repeat.repeat(" ", lengthOfTeamMAX - team.length())).append(time);
 			System.out.println(view);
 
 			if (key.equals("15. ")) {
-				System.out.println(repeat.repeat("-", view.toCharArray().length));
+				System.out.println(repeat.repeat("-", view.length()));
 			}
 
 		}

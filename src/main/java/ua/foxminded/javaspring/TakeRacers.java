73ce50f487d246ad49e3c6da74e272437027f878
”Kyrylo <kirillartemov1993@gmail.com>
2023-03-27 16:25:26 +0200

    recoded:
    removed 'for' loop
    method not take parametric
    changed try to try-with-resources

--- src/main/java/ua/foxminded/javaspring/TakeRacers.java
+++ src/main/java/ua/foxminded/javaspring/TakeRacers.java
@@ -4,35 +4,27 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class TakeRacers {
 
-	Map<String, ArrayList<String>> racers = new HashMap<>();
+	public Map<String, List<String>> takeRacers() throws FileNotFoundException {
+		Map<String, List<String>> racers = new HashMap<>();
 
-	public Map<String, ArrayList<String>> takeRacers(String fileWithNameOfRacers) throws FileNotFoundException {
-		Scanner sc = null;
-		try {
-			sc = new Scanner(new File(fileWithNameOfRacers));
+		try (Scanner sc = new Scanner(new File("src/main/resources/abbreviations.txt"))) {
 			while (sc.hasNextLine()) {
 				String[] racer = sc.nextLine().split("_");
-				for (int j = 0; j < racer.length; j++) {
-					ArrayList<String> nameAndCarOfRacer = new ArrayList<>();
-					nameAndCarOfRacer.add(racer[1]);
-					nameAndCarOfRacer.add(racer[2]);
-					racers.put(racer[0], nameAndCarOfRacer);
-				}
-			}
-		} catch (FileNotFoundException e) {
-		} catch (NoSuchElementException e) {
-		} finally {
-			if (sc != null) {
-				sc.close();
+				String key = racer[0];
+				String name = racer[1];
+				String team = racer[2];
+
+				racers.computeIfAbsent(key, k -> new ArrayList<>()).add(name);
+				racers.computeIfAbsent(key, k -> new ArrayList<>()).add(team);
 			}
 		}
-		return racers;
 
+		return racers;
 	}
 }
